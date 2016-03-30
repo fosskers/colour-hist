67cafda005ebf6831f804b67b982ef5d49caa768
Colin Woodbury <colingw@gmail.com>
2016-03-30 15:35:52 -0700

    Only read image files once in tests

--- test/Test.hs
+++ test/Test.hs
@@ -12,8 +12,10 @@ import           Test.Tasty.QuickCheck as QC
 
 ---
 
-suite :: TestTree
-suite = testGroup "Unit Tests"
+type H = YCbCrHist
+
+suite :: H -> H -> H -> H -> H -> H -> TestTree
+suite cat catT catO catB cali calB = testGroup "Unit Tests"
   [ testGroup "binNxN Sanity Checks"
     [ testCase "(0,0) -> (0,0)" $ (0,0) @?= bin8x8 (0,0)
     , testCase "(255,255) -> (7,7)" $ (7,7) @?= bin8x8 (255,255)
@@ -28,66 +30,31 @@ suite = testGroup "Unit Tests"
     ]
   , testGroup "intersection"
     [ testGroup "Image matched to tranformed self"
-      [ testCase "Same Model and Image: > 0.99" selfId
-      , testCase "Rotation: > 0.99" turned
-      , testCase "Occlusion: > 0.75" occlusion
-      , testCase "Model is twice as small: > 0.95" smaller
-      , testCase "Model is twice as large: > 0.9" bigger
+      [ testCase "Same Model and Image: > 0.99" $ two (> 0.99) cat cat
+      , testCase "Rotation: > 0.95" $ two (> 0.95) catT cat
+      , testCase "Occlusion: > 0.75" $ two (> 0.75) catO cat
+      , testCase "Model is twice as small: > 0.99" $ two (> 0.99) catB cat
+      , testCase "Model is twice as large: ~0.25" $ two (> 0.249) cat catB
       ]
     , testGroup "Image vs matching and unmatching models"
-      [ testCase "Same image" same
-      , testCase "Occlusion" occlusion3
-      , testCase "Model is smaller" smaller3
-      , testCase "Model is larger" bigger3
+      [ testCase "Same image" $ three cat cat cali
+      , testCase "Rotation" $ three catT cat cali
+      , testCase "Occlusion" $ three catO cat cali
+      , testCase "Model is smaller" $ three catB cat cali
+      , testCase "Model is larger" $ three cat catB calB
       ]
     ]
   ]
 
-two :: (Float -> Bool) -> FilePath -> FilePath -> Assertion
-two n f1 f2 = do
-  m <- ycbcrHist bin8x8 <$> readImg f1
-  i <- ycbcrHist bin8x8 <$> readImg f2
-  let intr = intersection i m
-  assertBool ("Intersection was: " ++ show intr) $ n intr
+two :: (Float -> Bool) -> H -> H -> Assertion
+two n i m = assertBool ("Intersection was: " ++ show intr) $ n intr
+  where intr = intersection i m
 
-three :: FilePath -> FilePath -> FilePath -> Assertion
-three f0 f1 f2 = do
-  m0 <- ycbcrHist bin32x32 <$> readImg f0
-  m1 <- ycbcrHist bin32x32 <$> readImg f1
-  i <- ycbcrHist bin32x32 <$> readImg f2
-  let intr0 = intersection i m0
-      intr1 = intersection i m1
-  assertBool ("Real: " ++ show intr0 ++ " | False: " ++ show intr1) $ intr0 > intr1
-
--- | An image should have a perfect match to itself.
-selfId :: Assertion
-selfId = two (> 0.99) "test/cat.jpg" "test/cat.jpg"
-
--- | An image should have a high match to a rotated version of itself.
-turned :: Assertion
-turned = two (> 0.99) "test/cat.jpg" "test/cat-turned.jpg"
-
--- | An image should have a decent match to an occluded version of itself.
-occlusion :: Assertion
-occlusion = two (> 0.75) "test/cat.jpg" "test/cat-neko.jpg"
-
-occlusion3 :: Assertion
-occlusion3 = three "test/cat.jpg" "test/calico.jpg" "test/cat-neko.jpg"
-
-smaller :: Assertion
-smaller = two (> 0.95) "test/cat.jpg" "test/cat-big.jpg"
-
-smaller3 :: Assertion
-smaller3 = three "test/cat.jpg" "test/calico.jpg" "test/cat-big.jpg"
-
-bigger :: Assertion
-bigger = two (> 0.9) "test/cat-big.jpg" "test/cat.jpg"
-
-bigger3 :: Assertion
-bigger3 = three "test/cat-big.jpg" "test/calico-big.jpg" "test/cat.jpg"
-
-same :: Assertion
-same = three "test/cat.jpg" "test/calico.jpg" "test/cat.jpg"
+three :: H -> H -> H -> Assertion
+three i m0 m1 = assertBool msg $ intr0 > intr1
+  where intr0 = intersection i m0
+        intr1 = intersection i m1
+        msg = "Real: " ++ show intr0 ++ " | False: " ++ show intr1
 
 readImg :: FilePath -> IO (V.Vector Word8)
 readImg fp = ycbcr . decodeJpeg <$> B.readFile fp
@@ -107,4 +74,11 @@ evenDist1 = r @?= [(0,0),(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7)]
         v = [0.04,0.08,0.12,0.16,0.20,0.24,0.28,0.32]
 
 main :: IO ()
-main = defaultMain suite
+main = do
+  cat  <- ycbcrHist bin32x32 <$> readImg "test/cat.jpg"
+  catT <- ycbcrHist bin32x32 <$> readImg "test/cat-turned.jpg"
+  catO <- ycbcrHist bin32x32 <$> readImg "test/cat-neko.jpg"
+  catB <- ycbcrHist bin32x32 <$> readImg "test/cat-big.jpg"
+  cali <- ycbcrHist bin32x32 <$> readImg "test/calico.jpg"
+  calB <- ycbcrHist bin32x32 <$> readImg "test/calico-big.jpg"
+  defaultMain (suite cat catT catO catB cali calB)
